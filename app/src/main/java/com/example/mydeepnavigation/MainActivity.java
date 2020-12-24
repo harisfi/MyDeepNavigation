2b22fb7d2552d69a8eeb5b311d68f64d3cc017d5
harisfi <acc.cp.haris@gmail.com>
2020-12-24 09:00:19 +0700

    Initial commit

---
+++ app/src/main/java/com/example/mydeepnavigation/MainActivity.java
@@ -0,0 +1,79 @@
+package com.example.mydeepnavigation;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.content.ContextCompat;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnOpenDetail = findViewById(R.id.btn_open_detail);
+        btnOpenDetail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent detailIntent = new Intent(MainActivity.this, DetailActivity.class);
+                detailIntent.putExtra(DetailActivity.EXTRA_TITLE, "Hola, Good News");
+                detailIntent.putExtra(DetailActivity.EXTRA_MESSAGE, "Now you can learn android in Dicoding");
+                startActivity(detailIntent);
+            }
+        });
+
+        showNotification(MainActivity.this, "Hi, how are you?", "Do you have any plan this weekend? Let\\'s hangout", 110);
+    }
+
+    private void showNotification(Context context, String title, String message, int notifId) {
+        String CHANNEL_ID = "Channel_1";
+        String CHANNEL_NAME = "Navigation channel";
+        Intent notifDetailIntent = new Intent(this, DetailActivity.class);
+        notifDetailIntent.putExtra(DetailActivity.EXTRA_TITLE, title);
+        notifDetailIntent.putExtra(DetailActivity.EXTRA_MESSAGE, message);
+        PendingIntent pendingIntent = TaskStackBuilder.create(this)
+                .addParentStack(DetailActivity.class)
+                .addNextIntent(notifDetailIntent)
+                .getPendingIntent(110, PendingIntent.FLAG_UPDATE_CURRENT);
+        NotificationManager notificationManagerCompat = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        Uri alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID)
+                .setContentTitle(title)
+                .setSmallIcon(R.drawable.ic_email)
+                .setContentText(message)
+                .setColor(ContextCompat.getColor(context, android.R.color.black))
+                .setVibrate(new long[]{1000, 1000, 1000, 1000, 1000})
+                .setSound(alarmSound)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID,
+                    CHANNEL_NAME,
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            channel.enableVibration(true);
+            channel.setVibrationPattern(new long[]{1000, 1000, 1000, 1000, 1000});
+            builder.setChannelId(CHANNEL_ID);
+            if (notificationManagerCompat != null) {
+                notificationManagerCompat.createNotificationChannel(channel);
+            }
+        }
+        Notification notification = builder.build();
+        if (notificationManagerCompat != null) {
+            notificationManagerCompat.notify(notifId, notification);
+        }
+    }
+}
